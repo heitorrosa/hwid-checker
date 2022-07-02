684cc72de6e4c6359a36c5cce46176b3b3577f35
Heitor Rosa <76885858+heitorrosa@users.noreply.github.com>
2022-07-02 13:39:08 -0300

    •  Reupoading Git|Hub is dying

---
+++ HWID Checker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HWID_Checker
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
